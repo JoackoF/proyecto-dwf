fb40d88eb1674d5e4ee1a3da5d9c0c9f8a2c43ee
Centeno Ramos <Centenojesus426@gmail.com>
2026-05-16 16:49:41 -0600

    excepcion personalizada y metodos

--- src/main/java/udb/edu/sv/exception/GlobalExceptionHandler.java
+++ src/main/java/udb/edu/sv/exception/GlobalExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import udb.edu.sv.dto.ApiResponse;
 import udb.edu.sv.util.ResponseBuilder;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
@@ -34,5 +36,21 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiResponse<Object>> handleGeneral(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ResponseBuilder.error("An unexpected internal server error occurred"));
+    }
+
+    //para que devuelva un mensaje si no ingresamos correctamente los valores
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    //para que nos devuelva un mensaje si no existe un usuario que busquemos
+    @ExceptionHandler(UsuarioNoEncontradoException.class)
+    public ResponseEntity<String> handleUsuarioNoEncontrado(UsuarioNoEncontradoException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("El usuario no se ha encontrado: " + ex.getMessage());
     }
 }
